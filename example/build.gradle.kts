a16faa51d973d9ba0e26b730970439ca41247f43
Ram-Desktop <tahor.ram@gmail.com>
2025-03-05 23:16:38 +0200

    Remove Material dependencies from library

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.material)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- swipeable_cards/build.gradle.kts
+++ swipeable_cards/build.gradle.kts
@@ -41,12 +41,11 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.material3)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
 }
 
 publishing {
